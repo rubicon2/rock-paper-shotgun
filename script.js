83c7dacd81274acc0af77317bed636a337daa357
Liam <liargent@hotmail.com>
2022-04-03 19:47:47 +0100

    Add ui and refactor code to work with js events.

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const shotgun = document.getElementById("shotgun");
+const info = document.getElementById("info");
+const gameStatus = document.getElementById("game-status");
+const playerScoreInfo = document.getElementById("player-score");
+const computerScoreInfo = document.getElementById("computer-score");
+
+const roundsToWin = 5;
+
 let humanWins = 0;
 let computerWins = 0;
-
-let keepGoing = true;
-let roundCount = 0;
 
 let weapons = ["rock", "paper", "shotgun"];
 
 let playerInput = null;
 let inputPromptMessage = { promptText: "Choose: rock, paper, shotgun!", default: "Shockypaperotgun" };
+
+function setInfoTexts() {
+  rock.weaponName = "rock";
+  paper.weaponName = "paper";
+  shotgun.weaponName = "shotgun";
+}
+
+function activateStartGameButton() {
+  gameStatus.textContent = "START GAME";
+  gameStatus.classList.add("selectable");
+  gameStatus.id = "game-status";
+  gameStatus.addEventListener("click", initialiseGame);
+}
+
+function deactivateStartGameButton() {
+  gameStatus.textContent = "";
+  gameStatus.classList.remove("selectable");
+  gameStatus.id = "";
+  gameStatus.removeEventListener("click", initialiseGame);
+}
+
+function activateWeaponInput() {
+  rock.addEventListener("click", runRound);
+  paper.addEventListener("click", runRound);
+  shotgun.addEventListener("click", runRound);
+  rock.classList.add("selectable");
+  paper.classList.add("selectable");
+  shotgun.classList.add("selectable");
+}
+
+function deactivateWeaponInput() {
+  rock.removeEventListener("click", runRound);
+  paper.removeEventListener("click", runRound);
+  shotgun.removeEventListener("click", runRound);
+  rock.classList.remove("selectable");
+  paper.classList.remove("selectable");
+  shotgun.classList.remove("selectable");
+}
 
 function getRangedRandomInt(min, max) {
   let range = max - min;
@@ -18,111 +63,82 @@ function initialiseGame() {
   roundCount = 0;
   humanWins = 0;
   computerWins = 0;
+  info.textContent = "Choose rock, paper, or shotgun!";
+  updateScores();
+  deactivateStartGameButton();
+  activateWeaponInput();
 }
 
-function initialiseRound() {
-  playerInput = null;
-  inputPromptMessage = { promptText: "Choose: rock, paper, shotgun!", default: "Shockypaperotgun" };
-}
+function runRound(e) {
 
-function validateInput(input) {
-  input = input.toLowerCase();
-  switch (input) {
-    case "rock": 
-    case "r": 
-      return "rock"; 
-    case "paper": 
-    case "p": 
-      return "paper";
-    case "shotgun":
-    case "s":
-      return "shotgun";
-    default:
-      return null;
-  }
-}
+  let playerChoice = e.currentTarget.weaponName;
+  let computerChoice = weapons[getRangedRandomInt(0, weapons.length - 1)];
 
-function checkRestartInput(input) {
-  input = input.toLowerCase();
-  switch (input) {
-    case "aw yeah man, that was absolutely incredible.":
-    case "aw yeah man, that was absolutely incredible":
-    case "aw yeah man":
-    case "yes":
-    case "y":
-      // keepGoing remains true and the game restarts. 
-      keepGoing = true;
-      break;
-    default:
-      keepGoing = false;
-  }
-}
-
-function runRound(playerChoice, computerChoice) {
   switch (playerChoice) {
     case "rock":
       if (computerChoice === "shotgun") {
-        alert("Your opponent fires a buckshot from their shotgun and it rebounds off your rock - you win the round!");
+        setInfo("Your opponent fires a buckshot from their shotgun and it rebounds off your rock - you win the round!");
         humanWins++;
       } else if (computerChoice === "paper") {
-        alert("You got wrapped up in paper and suffocated to death. Oof.");
+        setInfo("You got wrapped up in paper and suffocated to death. Oof.");
         computerWins++;
       } else {
-        alert("The rocks rub up against each other and nothing much happens. Guess it's a draw..."); 
+        setInfo("The rocks rub up against each other and nothing much happens. Guess it's a draw..."); 
       }
       break;
     case "paper":
       if (computerChoice === "rock") {
-        alert("You win to the max! There's no way the rock can defend the fluid curves of a piece of paper.");
+        setInfo("You win to the max! There's no way the rock can defend the fluid curves of a piece of paper.");
         humanWins++;
       } else if (computerChoice === "shotgun") {
-        alert("Your opponent fires a shell and your paper shield is shredded into nothing. Oof.");
-        computerChoice++;
+        setInfo("Your opponent fires a shell and your paper shield is shredded into nothing. Oof.");
+        computerWins++;
       } else {
-        alert("The paper rubs up against another piece of paper and they sort of crinkle up, but not enough to determine a victor. It's a draw...");
+        setInfo("The paper rubs up against another piece of paper and they sort of crinkle up, but not enough to determine a victor. It's a draw...");
       }
       break;
     case "shotgun":
       if (computerChoice === "rock") {
-        alert("You fire a buckshot from your shotgun and it rebounds off your opponent's rock. It blasts back into your face - you lose!");
+        setInfo("You fire a buckshot from your shotgun and it rebounds off your opponent's rock. It blasts back into your face - you lose!");
         computerWins++;
       } else if (computerChoice === "paper") {
-        alert("You fire a shot which tears through your opponent's paper shield. You win!");
+        setInfo("You fire a shot which tears through your opponent's paper shield. You win!");
         humanWins++;
       } else {
-        alert("You fire upon each other and both die simultaneously like in a vintage multiplayer game of Halo. Guess it's a draw..."); 
+        setInfo("You fire upon each other and both die simultaneously like in a vintage multiplayer game of Halo. Guess it's a draw..."); 
       }
       break;
     default:
-      alert(`I have no idea what a ${playerChoice} is. Pick something legit next time.`);
+      setInfo(`I have no idea what a ${playerChoice} is. Pick something legit next time.`);
       break;
   }
+
+  updateScores();
+  checkScores();
 }
 
-// Keep going until the user quits at the end. 
-while(keepGoing) {
+function updateScores() {
+  playerScoreInfo.textContent = `PLAYER: ${humanWins}`;
+  computerScoreInfo.textContent = `SKYNET: ${computerWins}`;
+}
 
-  initialiseGame(); 
-
-  for (let round = 0; round < 5; round++) {
-    initialiseRound();
-
-    // Get input from player - will you choose rock, paper or shotgun? 
-    while (playerInput == null) {
-      playerInput = validateInput(prompt(inputPromptMessage.promptText, inputPromptMessage.default)); 
-      if (playerInput == null) {
-        inputPromptMessage.promptText = "No! I said choose a ROCK, PAPER, or SHOTGUN!!!";
-      }
-    }
-
-    // Now have valid input, run the round. 
-    runRound(playerInput, weapons[getRangedRandomInt(0, weapons.length - 1)]);
-  }
-
-  // We have escaped the while loop, so the human or computer must have won. 
-  if (humanWins > computerWins) {
-    checkRestartInput(prompt("Congrats! You won at the guessing game! Play again?", "Aw yeah man, that was absolutely incredible."));
-  } else {
-    checkRestartInput(prompt("Boo! The computer won. Skynet is real! Play again?", "Ummmmm yeah no thanks.")); 
+function checkScores() {
+  if (humanWins >= roundsToWin) {
+    endGame("Player wins!");
+  } else if (computerWins >= roundsToWin) {
+    endGame("Skynet wins!");
   }
 }
+
+function endGame(endText) {
+  deactivateWeaponInput();
+  activateStartGameButton();
+  setInfo(endText);
+}
+
+function setInfo(text) {
+  info.textContent = text;
+}
+
+setInfoTexts();
+activateStartGameButton();
